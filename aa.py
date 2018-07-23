1b49c23ac1d43bd9c10fef66ac555c73245448e3
itsmeashutosh43 <ashutosh.chapagain@student.ku.edu.np>
2018-07-23 16:06:05 +0545

    Delete aa.py

--- aa.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-root = Tk()
-canvas = Canvas(root,width=200,height=200,bg="white")
-canvas.grid()
-firstRect = canvas.create_rectangle(0,0,10,10,fill="red")
-secondRect = canvas.create_rectangle(5,5,15,15,fill="blue")
-
-root.mainloop()
